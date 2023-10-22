1b489b362bbab9384b9fd47c72547b769c03bcdb
PY-PROGRAM <106869289+PY-PROGRAM@users.noreply.github.com>
2023-10-22 12:19:58 +0000

    operations

--- 1_basics/03_conversionOperation.js
+++ 1_basics/03_conversionOperation.js
@@ -20,6 +20,38 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ************************************************** Operations **********************************************************************************
+
+let value = 3
+let negValue = -value;
+//console.log(negValue);
+
+let str1="hello"
+let str2=" Bipin"
+let str3=str1+str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(2+2+"1");
+
+// console.log(+true);
+// console.log(+" ");
+
+//let num1,  num2, num3
+
+// num1 = num2 = num3 = 4+4
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+// console.log(gameCounter);
+
+let num1=3
+let num2=5
+let num3= num1===num2
+console.log(num3);
 
